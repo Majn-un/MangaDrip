f2e5bb674afb4df34402bc47f63352978e71f541
Mohammed Hager <mohager154@gmail.com>
2020-07-21 13:10:14 -0500

    cookies implemented

--- app/src/main/java/com/example/mangadrip/Activity/Page_Activity.java
+++ app/src/main/java/com/example/mangadrip/Activity/Page_Activity.java
@@ -66,6 +66,7 @@ public class Page_Activity extends AppCompatActivity {
                             .connect(Chapter_URL)
                             .method(Connection.Method.POST)
                             .execute();
+
                     Map<String, String> cookies = res.cookies();
                     Log.d("Cookies",""+cookies);
                     Document doc = Jsoup.connect(Chapter_URL).cookies(cookies).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36").get();
